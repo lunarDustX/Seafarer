8b09f454130779e0dcfacf7ff3f55a96131f771f
lunarDustX <365877793@qq.com>
2022-01-29 23:44:01 +0800

    战斗原型

---
+++ Seafarer/Assets/Scripts/Level/DummyGadget.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EasyButtons;
+
+public class DummyGadget : MonoBehaviour
+{
+    public GameObject gadgetPrefab;
+    public void Die()
+    {
+        Destroy(this.gameObject);
+    }
+
+    [Button(name:"Preview")]
+    private void Preview()
+    {
+        GetComponent<SpriteRenderer>().sprite = gadgetPrefab.GetComponentInChildren<SpriteRenderer>().sprite;
+    }
+
+}
---
+++ Seafarer/Assets/Scripts/Level/Group.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EasyButtons;
+
+public class Group : MonoBehaviour
+{
+    [SerializeField]
+    private int suites;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [Button(name:"Add Suite")]
+    private void AddSuite()
+    {
+        var newSuite = new GameObject();
+        suites++;
+        newSuite.name = "Suite" + suites;
+        newSuite.transform.parent = this.transform;
+    }
+
+}
