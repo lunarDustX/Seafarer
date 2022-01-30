ee33cb8b5a72a50252fe5f3d0364a327a387c9ce
lunarDustX <365877793@qq.com>
2022-01-31 00:51:22 +0800

    决斗逻辑初版
    50%

---
+++ Seafarer/Assets/BattleMgr.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleMgr : MonoBehaviour
+{
+    public GameObject battleUI;
+
+    private bool inBattle;
+    private GameObject player;
+    private GameObject enemy;
+
+    private Combat playerCombat;
+    private Combat enemyCombat;
+
+    void Start()
+    {
+        inBattle = false;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public void EncounterEnemy(Combat _enemy)
+    {
+        if (_enemy && _enemy.triggerDuel)
+        {
+            StartBattle(_enemy);
+        }
+    }
+
+    void StartBattle(Combat _enemy)
+    {
+        enemyCombat = _enemy;
+        inBattle = true;
+
+        // 设置目标
+        player.GetComponent<Combat>().SetTarget(_enemy);
+        _enemy.SetTarget(player.GetComponent<Combat>());
+
+        // 打开战斗界面
+        battleUI.SetActive(true);
+
+    }
+
+    void Update()
+    {
+        if (inBattle)
+        {
+            if (enemyCombat.energy >= 1)
+            {
+                enemyCombat.Attack();
+            }
+
+            //if (play)
+        }
+    }
+}
---
+++ Seafarer/Assets/BattleUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleUI : MonoBehaviour
+{
+    public Image playerEnergyFill;
+    public Image enemyEnergyFill;
+
+    public Button attackBtn;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+                
+    }
+}
